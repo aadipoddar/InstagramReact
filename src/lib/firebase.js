edbb704c888a474027243fabcd44de8ad6f67ca9
Aadi Poddar <aadipoddarmail@gmail.com>
2021-08-25 20:42:58 +0530

    all

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,20 +2,16 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-
-
 const config = {
-    apiKey: "AIzaSyBBI5ommrl2LnlLiXaHorJ_KMk9YGbeCaY",
-    authDomain: "instagram-314cf.firebaseapp.com",
-    projectId: "instagram-314cf",
-    storageBucket: "instagram-314cf.appspot.com",
-    messagingSenderId: "553565025318",
-    appId: "1:553565025318:web:fb9a3ad5d5a613ffd2e2e7"
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
 };
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
-
-
 
 export { firebase, FieldValue };
