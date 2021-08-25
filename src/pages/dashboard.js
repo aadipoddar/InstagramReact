60c11ad7eb1de5e818f7ad86011bf1b9fb094d85
Aadi Poddar <aadipoddarmail@gmail.com>
2021-08-25 09:39:15 +0530

    Header Component

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -9,9 +9,9 @@ export default function Dashboard() {
     }, []);
 
     return (
-        <div className = "bg-gray-background">
+        <div className="bg-gray-background">
             <Header />
-            <div className = "grid">
+            <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
                 <Timeline />
                 <Sidebar />
             </div>
